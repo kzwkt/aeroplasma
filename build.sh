c441d05fc4423a6d15a4e3ec3870d5c79d1f4a13
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-05-25 15:32:45 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ echo "Compiling $(pwd)"
 rm -rf build-kf6
 cmake -B build-kf6 -DCMAKE_INSTALL_PREFIX=$CUR_DIR/pkg -DCMAKE_BUILD_TYPE=Release -G Ninja -DBUILD_KF6=ON .
 ninja -C build-kf6
-sudo ninja -C build-kf6 install
+ninja -C build-kf6 install
 echo "Done."
 done
 
@@ -40,5 +40,4 @@ ninja install
 echo "Done."
 done
 
-tar -czvf  aeroplasma.tar.gz $CUR_DIR/pkg
-
+tar -czvf $CUR_DIR/aeroplasma.tar.gz $CUR_DIR/pkg
