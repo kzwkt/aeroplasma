c6cbf57a14228a7aae50437a8404e3a218392be2
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-05-25 14:28:48 +0000

    Update and rename PKGBUILD to build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+#pacman -S cmake extra-cmake-modules ninja qt6-virtualkeyboard qt6-multimedia qt6-5compat plasma-wayland-protocols plasma5support kvantum base-devel
+#sh compile.sh
+#sh install_plasmoids.sh
